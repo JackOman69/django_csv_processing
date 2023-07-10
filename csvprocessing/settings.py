66c0623a812ec6382714584097e54f144342deb4
kusaches <gypg5872@gmail.com>
2023-07-10 19:32:25 +0300

    Fix the error with redis

--- csvprocessing/settings.py
+++ csvprocessing/settings.py
@@ -172,7 +172,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://localhost:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
